e18848a87f7d47bffd3d074620db7f2b810ce401
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-13 00:27:22 -0800

    index

---
+++ operators/main.js
@@ -0,0 +1,11 @@
+let a=parseInt (prompt("Enter the number"));
+
+if(a>0){
+    document.write("The number is positive")
+}
+else if(a==0){
+    document.write("0 is neutral")
+}
+else{
+    document.write("The number is negative")
+}
