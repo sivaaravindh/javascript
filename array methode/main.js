5b7fd69b8e1d5ea99cbb44d78b61a3b72d157f97
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-18 20:00:34 -0800

    index

---
+++ array methode/main.js
@@ -0,0 +1,14 @@
+/*let x=prompt("enter the name")
+
+for(let i=1; i<=10; i++){
+
+    document.write(x + "<br>")
+    
+}*/
+
+let text =prompt("enter the name");
+let i = 1;
+while (i <=5) {
+    document.write(text + "<br>");
+  i++;
+}
